6b87de1ee22312a4dc41a41374c1552b7bcb679c
deb841 <62592670+deb841@users.noreply.github.com>
2025-03-30 01:53:47 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,7 @@
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_instance" "example" {
-  ami           = "ami-084568db4383264d4" # Change based on region
+  ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
+  subnet_id     = "subnet-095be9b1af8d1bcfc" # Replace with your subnet ID
 
   tags = {
     Name = "Terraform-GitHubActions-EC2"
