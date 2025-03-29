b152ae14761505d1343d9b8746885bbcab52f367
deb841 <62592670+deb841@users.noreply.github.com>
2025-03-30 01:44:44 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket-deb"
+    bucket         = "my-terraform-state-bucket-deb-123"
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
